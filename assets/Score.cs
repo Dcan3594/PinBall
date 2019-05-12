2954e0082fe14b9843e5a50996bbc40ee290bfb0
D_can3594 <drumcan3594@gmail.com>
2019-05-12 17:16:08 +0900

    PinBallプロジェクト Lesson5の課題
    発展課題未挑戦

---
+++ assets/Score.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText; //Text用変数
+    private int score = 0; //スコア計算用変数
+
+    // Use this for initialization
+    void Start()
+    {
+        score = 0;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        string yourTag = collision.gameObject.tag;
+
+        if (yourTag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (yourTag == "LargeStarTag")
+        {
+            score += 50;
+        }
+        else if (yourTag == "SmallCloudTag")
+        {
+            score += 30;
+        }
+        else if (yourTag == "LargeCloudTag")
+        {
+            score += 100;
+        }
+
+        SetScore();
+    }
+
+    void SetScore()
+    {
+        scoreText.text = string.Format("スコア:{0}", score);
+    }
+}
